7bfc275550e0eba2237123f37a1024c77a34a8f9
Hanju Kim <hanjukim@styleket.com>
2018-04-24 19:37:22 +0900

    Updated test

--- test.js
+++ test.js
@@ -1,18 +1,37 @@
 const test = require('ava');
-const fn = require('./index');
+const fn = require('.');
 
 test(t => {
-  t.true(fn({'foo-bar': true}).fooBar);
+  t.deepEqual(
+    fn({'foo-bar': true}),
+    { fooBar: true }
+  );
 });
 
 test(t => {
-  t.true(fn({"Test" : [0,1]}).test.length>0);
+  t.deepEqual(
+    fn({"Test" : [0,1]}),
+    { test: [0,1] }
+  );
 });
 
 test(t => {
-  t.true(fn({'foo-bar': {'bar-foo' : true}}).fooBar.barFoo);
+  t.deepEqual(
+    fn({'foo-bar': {'bar-foo' : true}}),
+    { fooBar: { barFoo: true } }
+  );
 });
 
 test(t => {
-  t.true(fn({'foo-bar': {'bar-foo' : [{'Test': true}]}}).fooBar.barFoo[0].test);
+  t.deepEqual(
+    fn({'foo-bar': {'bar-foo' : [{'Test': true}]}}),
+    {'fooBar': {'barFoo' : [{'test': true}]}}
+  );
+});
+
+test('accepts an array of objects', t => {
+  t.deepEqual(
+    fn([{foo_bar: true}, {bar_foo: false}, {'bar-foo': 'false'}]),
+    [{fooBar: true}, {barFoo: false}, {barFoo: 'false'}]
+  );
 });
